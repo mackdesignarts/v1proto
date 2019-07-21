54fb5983eeb51bb69513f0ecfe2ff3004417a957
Tony McLaughlin <tonymclaughlin72@gmail.com>
2019-07-21 11:05:45 -0400

    Updated CoreUI pkg

--- v1-client/src/containers/DefaultContainer.vue
+++ v1-client/src/containers/DefaultContainer.vue
@@ -3,12 +3,14 @@
     <AppHeader fixed>
       <SidebarToggler class="d-lg-none" display="md" mobile />
       <b-link class="navbar-brand" to="#">
-        <img class="navbar-brand-full" src="img/brand/logo.png" alt="V1 Voip logo">
+        <img class="navbar-brand-full" src="img/brand/logo.svg" width="89" height="25" alt="CoreUI Logo">
+        <img class="navbar-brand-minimized" src="img/brand/sygnet.svg" width="30" height="30" alt="CoreUI Logo">
       </b-link>
-      <img src="img/icons/hamburger icon.png" alt="toggle icon" />
       <SidebarToggler class="d-md-down-none" display="lg" :defaultOpen=true />
       <b-navbar-nav class="d-md-down-none">
-        <b-nav-item class="px-3" to="/support">Support</b-nav-item>
+        <b-nav-item class="px-3" to="/dashboard">Dashboard</b-nav-item>
+        <b-nav-item class="px-3" to="/users" exact>Users</b-nav-item>
+        <b-nav-item class="px-3">Settings</b-nav-item>
       </b-navbar-nav>
       <b-navbar-nav class="ml-auto">
         <b-nav-item class="d-md-down-none">
