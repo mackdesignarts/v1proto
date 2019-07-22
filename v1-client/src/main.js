8c3eac8f70d54f329d896ddd1032db2b4739b53c
Tony McLaughlin <tonymclaughlin72@gmail.com>
2019-07-22 18:19:14 -0400

    Added Vuex

--- v1-client/src/main.js
+++ v1-client/src/main.js
@@ -3,11 +3,14 @@ import 'core-js/es6/string'
 import 'core-js/es7/array'
 // import cssVars from 'css-vars-ponyfill'
 import Vue from 'vue'
+import Vuex from 'vuex'
+import 'es6-promise/auto'
 import BootstrapVue from 'bootstrap-vue'
 import App from './App'
 import router from './router'
 
 Vue.use(BootstrapVue)
+Vue.use(Vuex)
 
 new Vue({
   el: '#app',
